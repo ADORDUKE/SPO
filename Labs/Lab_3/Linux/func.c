9c68a98ac84cef813b163501f15a2c40afa5074d
Artyom Dordyuk <56783145+ADORDUKE@users.noreply.github.com>
2020-05-15 09:52:34 +0300

    1

--- Labs/Lab_3/Linux/func.c
+++ Labs/Lab_3/Linux/func.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 extern void func(const char path[]){
-	//chwon(path,getuid(),getgid());
 	if(remove(path)==0) printf("DONE\n");
 	else printf("ERROR\n");
 }
