c780e52cf172dde05f2c88083f19dc2a184340b8
Artem <a.dorduke@mail.ru>
2020-05-15 09:14:39 +0300

    lab3

---
+++ Labs/Lab_3/Windows/workdll.cpp
@@ -0,0 +1,22 @@
+﻿// workdll.cpp : Определяет экспортированные функции для приложения DLL.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Header.h"
+
+
+std::string func(std::string text,int n)
+{
+	std::string input = text;
+	std::string result = "";
+	for (int i = 0; i < input.length(); i += n)
+	{
+		result += input[i];
+	}
+	return result;
+
+}
+
+
